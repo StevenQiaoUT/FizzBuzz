83cbb3e19f1ba4352c4954e6e613c0ad1aeddb47
StevenQiaoUT <steven.qiao@mail.utoronto.ca>
2024-09-09 10:34:59 -0400

    Heil Kaiser dir

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.main(args);
         FizzBuzz.doFizzBuzz(5);
+        System.out.println("Heil Kaiser dir!");
     }
 }
