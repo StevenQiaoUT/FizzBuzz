d6e8ecb4d33350ccd4ddef5fa600e5caf5469317
Steven Qiao <steven.qiao@mail.utoronto.ca>
2024-09-11 23:55:21 -0400

    Completed Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,15 @@
 public class Reduce {
     public static void main(String[] args) {
-
+        int steps = 0;
+        int begin = 100;
+        while (begin > 0) {
+            if (begin % 2 == 0) {
+                begin /= 2;
+            } else {
+                begin--;
+            }
+            steps++;
+        }
+        System.out.println(steps);
     }
 }
